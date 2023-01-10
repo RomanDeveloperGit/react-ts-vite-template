156e1c9d1c3dd22dcc653a591192ca0181df0be9
RomanDeveloperGit <89508006+RomanDeveloperGit@users.noreply.github.com>
2023-01-10 20:06:08 +0500

    Updated information for .env

---
+++ src/env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+  // readonly APP_API_PORT: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="vite/client" />
-
-// interface ImportMetaEnv {
-//   readonly APP_API_PORT: string;
-// }
-
-// interface ImportMeta {
-//   readonly env: ImportMetaEnv;
-// }
